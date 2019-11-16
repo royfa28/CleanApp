b719cedd5b6f5d7be4511380a654e12db5da6c0f
alexandre <akekiwy@gmail.com>
2019-11-16 14:13:08 +1100

    login-register-module complet but weak check on login

--- app/src/main/java/com/example/cleanapp/MainActivity.java
+++ app/src/main/java/com/example/cleanapp/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.cleanapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.internal.zzdym;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseUserMetadata;
+import com.google.firebase.auth.UserInfo;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public EditText editTextMail,editTextPassword;
     public Button btnLogin, btnRegister;
     FirebaseAuth  myFirebaseAuth;
+    private FirebaseAuth.AuthStateListener myAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +40,98 @@ public class MainActivity extends AppCompatActivity {
         editTextPassword = findViewById(R.id.editTextPassword);
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
+        myFirebaseAuth = FirebaseAuth.getInstance();
+
+        myAuthStateListener = new FirebaseAuth.AuthStateListener()
+        {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth)
+            {
+                FirebaseUser myFirebaseUser = myFirebaseAuth.getCurrentUser();
+                if(myFirebaseUser != null)// weak no password check
+                {
+                    Toast.makeText(MainActivity.this, "LOGIN !",Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this,HomeActivity.class);
+                    startActivity(i);
+                }
+                else
+                    {
+                        Toast.makeText(MainActivity.this, "plz, Login ",Toast.LENGTH_SHORT).show();
+                    }
+
+            }
+        };
+
+        btnRegister.setOnClickListener(new View.OnClickListener()
+        {
+
+                @Override
+                public void onClick(View v)
+                {
+                    String mail = editTextMail.getText().toString();
+                    String password = editTextPassword.getText().toString();
+
+                    if( mail.isEmpty() )
+                    {
+                        editTextMail.setError("please enter an email");
+                        editTextMail.requestFocus();
+                    }
+                    else if(password.isEmpty())
+                    {
+                        editTextPassword.setError("password missing");
+                        editTextPassword.requestFocus();
+                    }
+                    else if(mail.isEmpty() && password.isEmpty())
+                    {
+                        Toast.makeText(MainActivity.this,"field are empty",Toast.LENGTH_SHORT).show();
+                    }
+                    else if (!(mail.isEmpty() && password.isEmpty()))
+                    {
+                        myFirebaseAuth.signInWithEmailAndPassword(mail,password).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>()
+                        {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task)
+                            {
+                                if(!task.isSuccessful())
+                                {
+                                    Toast.makeText(MainActivity.this, "Login Error",Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                    {
+                                        Intent toHome = new Intent(MainActivity.this,HomeActivity.class);
+                                        startActivity(toHome);
+                                    }
+
+                            }
+                        });
+                    }
+                    else
+                    {
+                        Toast.makeText(MainActivity.this, "Error",Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            });
+
+        btnRegister.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intoRegister = new Intent(MainActivity.this,registerActivity.class);
+                startActivity(intoRegister);
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myFirebaseAuth.addAuthStateListener(myAuthStateListener);
     }
 }
+
--- app/src/main/java/com/example/cleanapp/registerActivity.java
+++ app/src/main/java/com/example/cleanapp/registerActivity.java
@@ -1,14 +1,98 @@
 package com.example.cleanapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class registerActivity extends AppCompatActivity {
 
+    public EditText editTextEmail,editTextPassword;
+    public Button btnLogin, btnRegister;
+    public TextView txtValrAcc;
+    FirebaseAuth  myFirebaseAuth;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        myFirebaseAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        txtValrAcc = findViewById(R.id.textViewAlrAcc);
+
+        btnRegister.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String mail = editTextEmail.getText().toString();
+                String password = editTextEmail.getText().toString();
+
+                if( mail.isEmpty() )
+                {
+                    editTextEmail.setError("please enter an email");
+                    editTextEmail.requestFocus();
+                }
+                else if(password.isEmpty())
+                {
+                    editTextPassword.setError("password missing");
+                    editTextPassword.requestFocus();
+                }
+                else if(mail.isEmpty() && password.isEmpty())
+                {
+                    Toast.makeText(registerActivity.this,"field are empty",Toast.LENGTH_SHORT).show();
+                }
+                else if (!(mail.isEmpty() && password.isEmpty()))
+                {
+                    myFirebaseAuth.createUserWithEmailAndPassword(mail,password).addOnCompleteListener(registerActivity.this, new OnCompleteListener<AuthResult>()
+                    {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if(!task.isSuccessful())
+                            {
+                                Toast.makeText(registerActivity.this, "sign up fail",Toast.LENGTH_SHORT).show();
+                            }
+                            else
+                                {
+                                    startActivity(new Intent(registerActivity.this,HomeActivity.class));
+                                }
+                        }
+                    });
+                }
+                else
+                    {
+                        Toast.makeText(registerActivity.this, "Error",Toast.LENGTH_SHORT).show();
+                    }
+            }
+
+        });
+
+        txtValrAcc.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+             Intent i = new Intent(registerActivity.this,MainActivity.class);
+             startActivity(i);
+            }
+        });
     }
 }
+
