901ee9b783eec6af7fa90590339c2b366e0bfc3a
Roy Felix <roy.felix1609@gmail.com>
2019-12-05 11:51:09 +1100

    Change add house from fragment into activity

--- app/src/main/java/com/example/cleanapp/OwnerHomeFragment.java
+++ app/src/main/java/com/example/cleanapp/OwnerHomeFragment.java
@@ -4,28 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class OwnerHomeFragment extends Fragment {
+public class OwnerHomeFragment extends AppCompatActivity {
 
     public String userid;
     TextView userID;
 
     @Nullable
-    @Override
+    //@Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_owner_homepage, container, false);
 
-        Bundle bundle = getArguments();
+        //Bundle bundle = getArguments();
 
-        final OwnerMainActivity activity = (OwnerMainActivity)getActivity();
+        //final OwnerMainActivity activity = (OwnerMainActivity)getActivity();
+
+        Button addButton;
+        addButton = view.findViewById(R.id.button2);
+
+        addButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                getSupportActionBar().setTitle("Profile");
+            }
+        });
 
         /*
         Bundle results = activity.getUserID();
