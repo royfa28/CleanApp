d49e0fdc0fa5d93293139e527da6ca54c7b089b1
Roy <roy.felix1609@gmail.com>
2019-12-12 07:04:55 +1100

    Trying to insert into firebase (Not working yet)

--- app/src/main/java/com/example/cleanapp/AddHouseActivity.java
+++ app/src/main/java/com/example/cleanapp/AddHouseActivity.java
@@ -5,26 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cleanapp.Database.FirebaseDatabaseHelper;
 import com.example.cleanapp.Model.Room;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AddHouseActivity extends AppCompatActivity {
 
     EditText bedroom_amount, bathroom_amount, room_amount;
     Integer bedroom, bathroom, rooms;
+    TextView label;
 
     Room room;
-//    FirebaseDatabase firebaseDatabase;
-//    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,23 +41,33 @@ public class AddHouseActivity extends AppCompatActivity {
         bedroom_amount = (EditText) findViewById(R.id.bedroom_amount);
         bathroom_amount = (EditText) findViewById(R.id.bathroom_amount);
         room_amount = (EditText) findViewById(R.id.room_amount);
+        label = (TextView) findViewById(R.id.textView3);
 
         room = new Room();
-//        databaseReference = FirebaseDatabase.getInstance().getReference("User");
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("User");
 
-        final String bedroom_value = bedroom_amount.getText().toString();
 
-//        bedroom = new Integer(bedroom_value).intValue();
 //        bathroom = Integer.parseInt(bathroom_amount.getText().toString());
 //        rooms = Integer.parseInt(room_amount.getText().toString());
 
         saveHouseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("/House");
+                bedroom = Integer.parseInt(bedroom_amount.getText().toString());
+                getValues();
+                databaseReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        databaseReference.child("User01").setValue(room);
+                        Toast.makeText(AddHouseActivity.this, "Data inserted", Toast.LENGTH_SHORT).show();
+                    }
 
-                myRef.setValue("Hello, World!");
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
 //                Intent intent = new Intent(AddHouseActivity.this, OwnerMainActivity.class);
 //                startActivity(intent);
             }
@@ -62,8 +78,9 @@ public class AddHouseActivity extends AppCompatActivity {
         room.setRoomName("Bedroom");
         room.setDescription("");
 
-//        String key = databaseReference.push().getKey();
-//        databaseReference.child(key).child("roomName").setValue("Bedroom");
-//        databaseReference.child(key).child("Description").setValue("Emtpy");
+        for(int i = 1; i <= bedroom ; i++){
+            room.setRoomName("Bedroom " + i);
+            room.setDescription("Empty");
+        }
     }
 }
