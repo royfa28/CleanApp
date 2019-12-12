449c5d86ae3811e17aaf2d42e0a0caf01a2e5448
alexandre <akekiwy@gmail.com>
2019-12-13 10:20:57 +1100

    new user class

---
+++ app/src/main/java/com/example/cleanapp/Model/UserModel.java
@@ -0,0 +1,41 @@
+package com.example.cleanapp.Model;
+
+public class UserModel {
+    Boolean userLvl;
+    String userMail;
+    String userPhone;
+
+    //Constructor
+    public UserModel() {
+    }
+
+    public UserModel(String userMail, String userPhone) {
+        this.userMail = userMail;
+        this.userPhone = userPhone;
+    }
+
+    //GETTER SETTER
+    public Boolean getUserLvl() {
+        return userLvl;
+    }
+
+    public void setUserLvl(Boolean userLvl) {
+        this.userLvl = userLvl;
+    }
+
+    public String getUserMail() {
+        return userMail;
+    }
+
+    public void setUserMail(String userMail) {
+        this.userMail = userMail;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+}
