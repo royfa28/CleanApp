8b7767a91d16a78102adb5fe7a35ff9d4f75ccc7
alexandre <akekiwy@gmail.com>
2020-01-11 15:18:59 +1100

    add tenant act / tenant list act

--- app/src/main/java/com/example/cleanapp/HouseInvitation.java
+++ app/src/main/java/com/example/cleanapp/HouseInvitation.java
@@ -3,12 +3,28 @@ package com.example.cleanapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toolbar;
 
 public class HouseInvitation extends AppCompatActivity {
+
+    protected Toolbar myToolbar;
+    protected EditText phoneNumberTenant;
+    protected Button validationBtn;
+    protected HouseInvitation myHouseInvitation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_invitation);
+
+        myToolbar = findViewById(R.id.toolbar2);
+        phoneNumberTenant = findViewById(R.id.editText);
+        validationBtn = findViewById(R.id.valdBtn);
+        myHouseInvitation = new HouseInvitation();
+        //get the info owner to fill house invitation model
+
     }
 }
